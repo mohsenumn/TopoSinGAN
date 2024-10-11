992a85a7d7bc5c027201861fbc04f4c9e8b02064
Mohsen Ahmadkhani <mohsen@Mohsens-MacBook-Pro-5.local>
2024-10-11 00:51:12 -0500

    added CREMI experiment

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -u toposingan/main_train.py --input_name agricultural_fields.png --nc_z 4 --nc_im 4 --gpu_id 0
+python -u toposingan/main_train.py --input_name cremi3.png --nc_z 4 --nc_im 4 --gpu_id 0
